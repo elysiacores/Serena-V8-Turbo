6b6a8b30248c6b7239d688a0bf02cb7f75f183a0
Nut Werayoot <elysia.cores@gmail.com>
2026-09-20 00:19:08 +0000

    chore: align benchmark lint on CI

--- benchmarks/phase10_integration.py
+++ benchmarks/phase10_integration.py
@@ -21,7 +21,7 @@ from concurrent.futures import Future
 V8_SRC = Path(__file__).resolve().parents[1] / "src"
 sys.path.insert(0, str(V8_SRC))
 
-from serena_v8._version import VERSION
+from serena_v8._version import VERSION  # noqa: E402
 
 
 class V8IntegrationTest:
